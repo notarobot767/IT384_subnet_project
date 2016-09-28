36493e6d93f77da4fb72c7e4405386d7fdba220e
Noah Ogrydziak <Nrogrydziak@gmail.com>
2016-09-27 21:37:33 -0400

    add comments to main and argpasre

--- subnet_package/main.py
+++ subnet_package/main.py
@@ -4,11 +4,11 @@ from .view.view import View
 from .misc import Misc
 
 def main(argc, argv):
-  _mod = Model(argv[0])
+  _mod = Model(argv[0]) #sending address block to main
   ctrl = Controller(_mod)
   view = View(ctrl)
 
-  if(argc == 2):
+  if(argc == 2): #checking if requirements argument was passed
     for name, hosts in argv[1]:
       ctrl.add_new_subnet(hosts, name, True)
 
--- subnet_package/model/model.py
+++ subnet_package/model/model.py
@@ -2,7 +2,6 @@ from .subnet_db import Subnet_DB
 
 class Model(object):
   def __init__(self, argv):
-    #self.tracker = IP_tracker("172.16.0.0/29") #test
     self.subnet_db = Subnet_DB(argv)
 
   def speak(self):
